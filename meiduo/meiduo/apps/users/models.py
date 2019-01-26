9105d5d44e37147f33b0fed849dc155617c9a161
weixiaoguo <xiao66guo@163.com>
2019-01-26 17:56:56 +0800

    在地址模型中添加默认地址字段

--- meiduo/meiduo/apps/users/models.py
+++ meiduo/meiduo/apps/users/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     """用户模型类"""
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
     email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+    default_address = models.ForeignKey('Address', related_name='users', null=True, blank=True,on_delete=models.SET_NULL, verbose_name='默认地址')
 
     class Meta:
         db_table = 'tb_users'
@@ -62,4 +63,4 @@ class Address(BaseModel):
         db_table = 'tb_address'
         verbose_name = '用户地址'
         verbose_name_plural = verbose_name
-        ordering = ['-update_time']
+        ordering = ['-update_time']     # 指定默认排序
